f0363ac344a0da71cc6bf893d72b67a910ffdad1
Nexo <gamingnight492@gmail.com>
2023-07-09 15:51:40 +0700

    add animation to Pesan Sekarang and Lanjut

--- src/Controller/CheckOutPageController.java
+++ src/Controller/CheckOutPageController.java
@@ -1,14 +1,19 @@
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import Model.Barang;
 import Model.User;
+import animatefx.animation.ZoomIn;
 import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -16,6 +21,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 public class CheckOutPageController implements Initializable{
@@ -90,6 +97,16 @@ public class CheckOutPageController implements Initializable{
     @FXML
     private Button Lanjutberburu;
 
+    @FXML
+    private Pane Overlay;
+
+    @FXML
+    private Pane Tail;
+
+    @FXML
+    private Pane Body;
+
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         DB.ConnectToDataBase("src/TokopaediDatabase.db");
@@ -121,16 +138,37 @@ public class CheckOutPageController implements Initializable{
 
     @FXML
     void Pesan(MouseEvent event) {
+        DB.ConnectToDataBase("src/TokopaediDatabase.db");
+        Barang IDBarang = new Barang();
+        Barang barang = DB.SelectBarang(IDBarang.getID());
 
         PesanDiTerimaDiskon.setText("Rp " + Diskon.getText().replace("Rp ", ""));
-        PesanDiTerimaGambar = Gambar;
+        PesanDiTerimaGambar.setImage(barang.getGambar());;
         PesanDiTerimaPengiriman.setText("Rp "+Pengiriman.getText().replace("Rp ", ""));
         PesanDiTerimaHarga.setText("Rp "+HargaProduk.getText().replace("Rp ", ""));
         PesanDiTerimaTotal.setText("Rp "+Total.getText().replace("Rp ", ""));
+
+        TranslateTransition tailer = new TranslateTransition(Duration.millis(1000), Tail);
+        TranslateTransition bodyier = new TranslateTransition(Duration.millis(1200), Body);
+        TranslateTransition overlayer = new TranslateTransition(Duration.millis(1000), Overlay);
+
         TranslateTransition GoToDiterima = new TranslateTransition(Duration.millis(1700), PesanDiTerima);
         TranslateTransition GoToDiterimaCard = new TranslateTransition(Duration.millis(2000), CardPesanDiterima);
         TranslateTransition ButtonDiTerima = new TranslateTransition(Duration.millis(2300), Lanjutberburu);
         GoToDiterima.setInterpolator(Custom1);
+        tailer.setInterpolator(Custom1);
+        bodyier.setInterpolator(Custom1);
+        overlayer.setInterpolator(Custom1);
+
+        tailer.setToX(1280);
+        bodyier.setDelay(Duration.millis(150));
+        bodyier.setToX(1280);
+        overlayer.setToX(1280);
+
+        tailer.setToX(1280);
+        bodyier.setDelay(Duration.millis(150));
+        bodyier.setToX(1280);
+        
         GoToDiterima.setToY(-800);
         GoToDiterimaCard.setDelay(Duration.millis(100));
         GoToDiterimaCard.setInterpolator(Custom1);
@@ -138,15 +176,55 @@ public class CheckOutPageController implements Initializable{
         ButtonDiTerima.setDelay(Duration.millis(50));
         ButtonDiTerima.setInterpolator(Custom1);
         ButtonDiTerima.setToY(-800);
+        
+        bodyier.setOnFinished(e -> Overlaygone());
 
         GoToDiterima.play();
         GoToDiterimaCard.play();
         ButtonDiTerima.play();
+        tailer.play();
+        overlayer.play();
+        bodyier.play();
+
+
+
+    }
+
+    private void Overlaygone(){
+        TranslateTransition tailer = new TranslateTransition(Duration.millis(1000), Tail);
+        TranslateTransition bodyier = new TranslateTransition(Duration.millis(1200), Body);
+        TranslateTransition overlayer = new TranslateTransition(Duration.millis(1000), Overlay);
+
+        tailer.setInterpolator(Custom1);
+        bodyier.setInterpolator(Custom1);
+        overlayer.setInterpolator(Custom1);
+
+        tailer.setToX(2560);
+        tailer.setDelay(Duration.millis(150));
+        bodyier.setToX(2560);
+        overlayer.setToX(2560);
+
+        tailer.play();
+        overlayer.play();
+        bodyier.play();
 
     }
 
     @FXML
-    void BackToMain(MouseEvent event) {
+    void BackToMain(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GraphicUserInterface/MainScreen.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setTitle("Tokopaedi");
+            stage.show();
+
+            new ZoomIn(root).setSpeed(2.2).play();
+
+            ((Parent) event.getSource()).getScene().getWindow().hide();
 
     }
 
