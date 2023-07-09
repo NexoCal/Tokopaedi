18d8e632b7f47be9e6d9e6fe3f0ef8b2f244845c
Nexo <gamingnight492@gmail.com>
2023-07-09 15:02:57 +0700

    add animation to Pesan Sekarang

--- src/Controller/CheckOutPageController.java
+++ src/Controller/CheckOutPageController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 import Model.Barang;
 import Model.User;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,7 +14,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class CheckOutPageController implements Initializable{
     DatabaseModel DB = new DatabaseModel();
@@ -56,6 +60,36 @@ public class CheckOutPageController implements Initializable{
     @FXML
     private HBox Userdetail;
 
+    @FXML
+    private Label Diskon;
+
+    @FXML
+    private Label Pengiriman;
+    
+    @FXML
+    private Pane PesanDiTerima;
+
+    @FXML
+    private Label PesanDiTerimaDiskon;
+
+    @FXML
+    private ImageView PesanDiTerimaGambar;
+
+    @FXML
+    private Label PesanDiTerimaHarga;
+
+    @FXML
+    private Label PesanDiTerimaTotal;
+
+    @FXML
+    private Label PesanDiTerimaPengiriman;
+
+    @FXML
+    private VBox CardPesanDiterima;
+
+    @FXML
+    private Button Lanjutberburu;
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         DB.ConnectToDataBase("src/TokopaediDatabase.db");
@@ -78,8 +112,41 @@ public class CheckOutPageController implements Initializable{
 
     }
 
+    Interpolator Custom1 = new Interpolator() {
+        @Override
+        protected double curve(double e) {
+            return (e == 1.0) ? 1.0 : 1 - Math.pow(2.0, -10 * e);
+        }
+    };
+
     @FXML
     void Pesan(MouseEvent event) {
+
+        PesanDiTerimaDiskon.setText("Rp " + Diskon.getText().replace("Rp ", ""));
+        PesanDiTerimaGambar = Gambar;
+        PesanDiTerimaPengiriman.setText("Rp "+Pengiriman.getText().replace("Rp ", ""));
+        PesanDiTerimaHarga.setText("Rp "+HargaProduk.getText().replace("Rp ", ""));
+        PesanDiTerimaTotal.setText("Rp "+Total.getText().replace("Rp ", ""));
+        TranslateTransition GoToDiterima = new TranslateTransition(Duration.millis(1700), PesanDiTerima);
+        TranslateTransition GoToDiterimaCard = new TranslateTransition(Duration.millis(2000), CardPesanDiterima);
+        TranslateTransition ButtonDiTerima = new TranslateTransition(Duration.millis(2300), Lanjutberburu);
+        GoToDiterima.setInterpolator(Custom1);
+        GoToDiterima.setToY(-800);
+        GoToDiterimaCard.setDelay(Duration.millis(100));
+        GoToDiterimaCard.setInterpolator(Custom1);
+        GoToDiterimaCard.setToY(-800);
+        ButtonDiTerima.setDelay(Duration.millis(50));
+        ButtonDiTerima.setInterpolator(Custom1);
+        ButtonDiTerima.setToY(-800);
+
+        GoToDiterima.play();
+        GoToDiterimaCard.play();
+        ButtonDiTerima.play();
+
+    }
+
+    @FXML
+    void BackToMain(MouseEvent event) {
 
     }
 
